f49caa32c0ae91d14763ca133fbcbf811c46d05c
Mateusz Zieliński <mateusz@roombelt.com>
2023-11-29 18:55:30 +0100

    Fix build

--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,7 @@ export { createState } from "./create-state";
 export { createComputed } from "./create-computed";
 export { createCollection } from "./create-collection";
 export { createQuery } from "./create-query";
-export { createExternalState, compute, ActiveState } from "./core";
+export { createExternalState, compute } from "./core";
 export { useSelector } from "./react";
+
+export type { ActiveState } from "./core";
