770b73611c30c69aed48d68d882847e0bb1226fa
Mateusz Zieliński <mateusz@roombelt.com>
2024-09-20 13:30:11 +0200

    Add onSubscribe to activeState

--- lib/create-state.ts
+++ lib/create-state.ts
@@ -1,31 +1,43 @@
-import { activeExternalState, Dependency } from "./core";
+import { activeExternalState } from "./core";
 
 export interface ActiveState<V> {
   type: "active-state";
   get: () => V;
   set: (value: V) => void;
-  subscribe: (listener: (dependency: Dependency) => any) => () => void;
+  subscribe: (listener: (value: V) => any) => () => void;
 }
 
-export function activeState<V>(initialValue: V) {
+export function activeState<V>(
+  initialValue: V,
+  options: {
+    onSubscribe?: (set: (value: V) => void, get: () => V) => () => void;
+  } = {}
+) {
   let value = initialValue;
+
+  function setValue(newValue: V) {
+    if (!Object.is(newValue, value)) {
+      value = newValue;
+      topic.notify();
+    }
+  }
 
   const topic = activeExternalState(
     () => value,
-    (emit) => () => null
+    (): (() => void) => {
+      if (options?.onSubscribe) {
+        return options?.onSubscribe(setValue, topic.get);
+      } else {
+        return () => null;
+      }
+    }
   );
 
-  const result: ActiveState<V> = {
-    type: "active-state" as const,
+  const result = {
     get: topic.get,
-    set(newValue: V) {
-      if (!Object.is(newValue, value)) {
-        value = newValue;
-        topic.notify();
-      }
-    },
-    subscribe: topic.subscribe,
+    set: setValue,
+    subscribe: (listener: (value: V) => void) =>
+      topic.subscribe(() => listener(topic.get())),
   };
-
   return result;
 }
