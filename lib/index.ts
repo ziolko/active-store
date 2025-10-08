8312aec19a64c680220b2dc5c3f58b3dd15426a1
Mateusz Zielinski <mateusz@roombelt.com>
2025-10-08 15:18:18 +0200

    Add gitignore

--- lib/index.ts
+++ lib/index.ts
@@ -2,8 +2,7 @@
 export { activeState as state } from "./create-state";
 export { activeAsync as query } from "./create-query";
 export { activeComputed as computed } from "./create-computed";
-export { getActive as get } from "./core";
-export { useActive as useStore, ActiveBoundary as Boundary } from "./react";
+export { useActive as useStore } from "./react";
 
 // Plumbing
 
@@ -11,4 +10,3 @@ export type { ActiveState as State } from "./create-state";
 export type { ActiveAsync as Query } from "./create-query";
 export type { ActiveComputed as Computed } from "./create-computed";
 export type { Active as Get, ActiveSubscribe as Subscribe } from "./core";
-export type { ActiveBoundaryProps as BoundaryProps, ActiveBoundaryErrorProps as BoundaryErrorProps } from "./react";
